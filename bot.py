6066bc47be75a5e588042ee6b953173d4afd6670
NingmengLemon <guanchenxiang@outlook.com>
2024-10-28 09:29:03 +0800

    feat(EroMoncak): Use pypinyin

--- bot.py
+++ bot.py
@@ -6,24 +6,35 @@ from melobot import Bot
 from melobot.log import Logger, LogLevel
 from melobot.protocols.onebot.v11 import Adapter, ForwardWebSocketIO
 
+from pydantic import BaseModel
+
+
+class ForwWsIOConfigModel(BaseModel):
+    url: str
+    access_token: str | None
+
+
+class GlobalConfigModel(BaseModel):
+    forwwsio: ForwWsIOConfigModel
+    debug: bool = False
+    plugins: list[str] = []
+
 
 debug = "--debug" in sys.argv
 
-with open("./config.json", "r", encoding="utf-8") as fp:
-    cfg: dict[str, Any] = json.load(fp)
+with open("./config.json", "rb") as fp:
+    cfg = GlobalConfigModel.model_validate_json(fp.read())
 if debug:
-    print("config content:", cfg)
+    print("config content:", cfg.model_dump_json(indent=4))
 
 if __name__ == "__main__":
     bot = (
         Bot(
             "MiraiChan",
-            logger=Logger(
-                level=LogLevel.DEBUG if debug else LogLevel.INFO
-            ),
+            logger=Logger(level=LogLevel.DEBUG if debug else LogLevel.INFO),
         )
-        .add_io(ForwardWebSocketIO(**cfg["forwwsio"]))
+        .add_io(ForwardWebSocketIO(**cfg.forwwsio.model_dump()))
         .add_adapter(Adapter())
     )
-    bot.load_plugins(cfg.get("plugins", []))
+    bot.load_plugins(cfg.plugins)
     bot.run(debug=debug)
