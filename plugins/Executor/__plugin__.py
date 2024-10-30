d37d593b137ca083e1cd83b30c8d6720391360a4
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2024-10-31 02:36:29 +0800

    update

--- plugins/Executor/__plugin__.py
+++ plugins/Executor/__plugin__.py
@@ -1,5 +1,5 @@
 import functools
-from asyncio import subprocess
+import subprocess
 
 from melobot import Plugin, get_logger, GenericLogger
 from melobot.protocols.onebot.v11 import on_message, on_start_match, Adapter
@@ -8,6 +8,18 @@ from melobot.session import enter_session, get_rule, suspend
 from melobot.session.option import Rule
 
 import checker_factory
+from lemony_utils.asyncutils import run_as_async
+
+
+def run_shell_command(command):
+    result = subprocess.run(
+        command,
+        shell=True,
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+    return result.stdout, result.stderr, result.returncode
 
 
 @on_start_match(".shell", checker=lambda e: e.sender.user_id == checker_factory.owner)
@@ -17,6 +29,16 @@ async def run_shell(event: MessageEvent, adapter: Adapter, logger: GenericLogger
     cmd = _[1].strip()
     if not cmd:
         return
+    await adapter.send_reply("开始运行指令，请坐和放宽...")
+    logger.debug(f"executing > {cmd}")
+    stdout, stderr, code = await run_as_async(run_shell_command, (cmd,))
+    reply = []
+    if s := stdout.strip():
+        reply.append(f"stdout:\n{s}")
+    if s := stderr.strip():
+        reply.append(f"stderr:\n{s}")
+    reply.append(f"\ncode = {code}")
+    await adapter.send_reply("\n".join(reply))
 
 
 @on_start_match(".pyexec", checker=lambda e: e.sender.user_id == checker_factory.owner)
