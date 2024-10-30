beac1f60ef8290c75cd5b90874b64bb6cffcf6c2
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2024-10-31 02:36:29 +0800

    update

--- lemony_utils/asyncutils.py
+++ lemony_utils/asyncutils.py
@@ -1,6 +1,9 @@
 import threading
 import asyncio
+from asyncio import subprocess
 import functools
+import sys
+import signal
 from typing import Optional, Mapping, Iterable, Any, Callable, Union
 
 
@@ -34,7 +37,9 @@ class ThreadWithReturn(threading.Thread):
             del self._target, self._args, self._kwargs
 
 
-async def run_as_async[T](
+async def run_as_async[
+    T
+](
     func: Callable[..., T],
     args=(),
     kwargs=None,
@@ -67,3 +72,57 @@ def run_as_async_decorator(daemon: bool = True, check_delay: Union[float, int] =
         return wrapper
 
     return decorator
+
+
+class NotStartedError(Exception):
+    pass
+
+
+class InteractiveProcess:
+    def __init__(self, cmd: str):
+        self._process: asyncio.subprocess.Process | None = None
+        self._cmd = cmd
+
+    async def start(self):
+        self._process = await asyncio.create_subprocess_shell(
+            cmd=self._cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    async def send(self, content: str, end: str = "\n"):
+        if self._process is None:
+            raise NotStartedError("call start() first before you act")
+        self._process.stdin.write((content + end).encode("utf-8", errors="replace"))
+        await self._process.stdin.drain()
+
+    async def readline(self, strip: bool = False):
+        if self._process is None:
+            raise NotStartedError("call start() first before you act")
+        output = (await self._process.stdout.readline()).decode(
+            "utf-8", errors="replace"
+        )
+        return output.strip() if strip else output
+
+    async def drain_output(self):
+        while True:
+            line = await self.readline(strip=False)
+            if not line:
+                break
+            print("drained:", line.strip())
+
+    async def close(self, force: bool = False):
+        if self._process is None:
+            raise NotStartedError("call start() first before you act")
+        if (code := self._process.returncode) is None:
+            if force:
+                if sys.platform == "win32":
+                    self._process.kill()
+                else:
+                    self._process.send_signal(signal.SIGKILL)
+            else:
+                self._process.send_signal(signal.SIGTERM)
+            code = await self._process.wait()
+        self._process = None
+        return code
