c3347290cbe522c77b4d0ede0c6e7c4e135a4bfa
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2024-11-19 21:28:33 +0800

    feat: MoeAttriLottery plugin

---
+++ src/plugins/MoeAttriLottery/__plugin__.py
@@ -0,0 +1,51 @@
+import os
+import time
+
+from pydantic import BaseModel
+
+from melobot import get_logger
+from melobot.plugin import Plugin
+from melobot.log import GenericLogger
+from melobot.protocols.onebot.v11.handle import on_command
+from melobot.protocols.onebot.v11.adapter import Adapter
+from melobot.protocols.onebot.v11.adapter.event import GroupMessageEvent
+
+from configloader import ConfigLoader, ConfigLoaderMetadata
+import checker_factory
+
+from .lottery import MoeLot
+
+
+class QuoteConfig(BaseModel):
+    moedata_file: str = "data/moe_attrs.json"
+
+
+os.makedirs("data/fonts", exist_ok=True)
+cfgloader = ConfigLoader(
+    ConfigLoaderMetadata(model=QuoteConfig, filename="moelottery_conf.json")
+)
+cfgloader.load_config()
+logger = get_logger()
+moelot = MoeLot(cfgloader.config.moedata_file)
+cd_table: dict[int, str] = {}
+
+
+@on_command(".", " ", ["今日人设", "萌抽签"])
+async def draw_attrs(event: GroupMessageEvent, adapter: Adapter, logger: GenericLogger):
+    if (
+        cd_table.get(event.sender.user_id, "")
+        == (now_date := time.strftime("%Y-%m-%d", time.localtime()))
+        and event.sender.user_id != checker_factory.owner
+    ):
+        await adapter.send_reply("今天已经抽过了噢，还要再抽一次吗w")
+        return
+    cd_table[event.sender.user_id] = now_date
+    moeattr = moelot.draw()
+    logger.debug(f"{moeattr=}")
+    await adapter.send_reply(f"你今天的人设是{moelot.to_text(moeattr)}！")
+
+
+class MoeLottery(Plugin):
+    author = "LemonyNingmeng"
+    version = "0.1.0"
+    flows = (draw_attrs,)
---
+++ src/plugins/MoeAttriLottery/lottery.py
@@ -0,0 +1,73 @@
+import json
+import random
+import time
+from typing import NotRequired, TypedDict
+
+
+class MoeData(TypedDict):
+    age: str
+    shallowchara: str
+    deepchara: str
+    habit: str
+    hairstyle: NotRequired[str]
+    haircolor: str
+    pupilcolor: str
+    breast: str
+    race: str
+    # 细节属性
+    racial_feature: NotRequired[str]
+    detailed_race: NotRequired[str]
+    detailed_pupilcolor: NotRequired[str]
+    # 记录时间
+    time: float
+
+
+class MoeLot:
+    def __init__(self, moefile: str):
+        with open(moefile, "r", encoding="utf-8") as fp:
+            data = json.load(fp)
+        self._moeattrs: dict[str, float] = data["moeattrs"]
+        self._d_moeattrs: dict[str, dict[str, float]] = data["detailed_moeattrs"]
+
+    @staticmethod
+    def random_with_weight(data_dict: dict[str, float]):
+        sum_wt = sum(data_dict.values())
+        ra_wt = random.uniform(0, sum_wt)
+        cur_wt = 0
+        for key in data_dict.keys():
+            cur_wt += data_dict[key]
+            if ra_wt <= cur_wt:
+                return key
+
+    def draw(self) -> MoeData:
+        res = {}
+        for attr_type in self._moeattrs.keys():
+            res[attr_type] = self.random_with_weight(self._moeattrs[attr_type])
+        for attr_type in self._d_moeattrs.keys():
+            for req_attr in self._d_moeattrs[attr_type].keys():
+                if req_attr in res.values():
+                    res[attr_type] = self.random_with_weight(
+                        self._d_moeattrs[attr_type][req_attr]
+                    )
+        for k in [k for k, v in res.items() if v in ("普通", "/", "无")]:
+            res.pop(k)
+        res["time"] = time.time()
+        return res
+
+    @staticmethod
+    def to_text(moedata: MoeData):
+        quantifier = "个"
+        match moedata["age"]:
+            case "幼女" | "萝莉" | "合法萝莉":
+                quantifier = "只"
+            case "少女" | "御姐" | "非法御姐":
+                quantifier = "位"
+        text = "一{quantifier}表面{shallowchara}、内里{deepchara}还带点{habit}的{haircolor}{pupilcolor}{breast}{age}{race}".format(
+            quantifier=quantifier, **moedata
+        )
+        if "racial_feature" in moedata:
+            text += f"，有着{moedata['racial_feature']}"
+        if "detailed_race" in moedata:
+            text += f"，具体地说是一{quantifier}{moedata['detailed_race']}"
+
+        return text
