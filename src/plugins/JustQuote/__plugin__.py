c878f15f4f11e3af7d2b8eef678e6a9320e27356
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2024-12-14 15:56:15 +0800

    feat: ability to update quote to group files (testing)

--- src/plugins/JustQuote/__plugin__.py
+++ src/plugins/JustQuote/__plugin__.py
@@ -1,21 +1,43 @@
 import base64
+from io import BytesIO
+import time
 import os
+from typing import cast
+
 from melobot import Plugin, get_logger
+from melobot.protocols.onebot.v11.adapter.echo import Echo
 from melobot.protocols.onebot.v11.adapter.event import GroupMessageEvent
 from melobot.protocols.onebot.v11.adapter.segment import ReplySegment, ImageSegment
 from melobot.protocols.onebot.v11 import on_command, Adapter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from configloader import ConfigLoader, ConfigLoaderMetadata
-from lagrange_extended_actions import UploadGroupFileAction
+from extended_actions.lagrange import (
+    UploadGroupFileAction,
+    GetGroupRootFilesAction,
+    CreateGroupFileFolderAction,
+)
 
 from .maker import QuoteMaker
+from .tmpfilehost import TmpfileHost
+
+qqface_to_emoji_map = {
+    
+}
+
+
+class UploaderCfg(BaseModel):
+    tmpfilehost: str | None = None
+    access_token: str = "哼哼哼啊啊"
+    enable: bool = False
+    folder: str = "群U的怪话"
 
 
 class QuoteConfig(BaseModel):
     emoji_cdn: str | None = None
     font: str = "data/fonts/NotoSansSC-Medium.ttf"
     mask: str = "data/quote_mask.png"
+    uploader: UploaderCfg = Field(default_factory=UploaderCfg)
 
 
 os.makedirs("data/fonts", exist_ok=True)
@@ -29,6 +51,15 @@ maker = QuoteMaker(
     bg_mask=cfgloader.config.mask,
     emoji_cdn=cfgloader.config.emoji_cdn,
 )
+tfh = (
+    TmpfileHost(
+        endpoint=cfgloader.config.uploader.tmpfilehost,
+        access_token=cfgloader.config.uploader.access_token,
+    )
+    if cfgloader.config.uploader.tmpfilehost and cfgloader.config.uploader.enable
+    else None
+)
+upload_folder = cfgloader.config.uploader.folder
 
 
 @on_command(".", " ", ["q", "quote"])
@@ -54,6 +85,70 @@ async def quote(adapter: Adapter, event: GroupMessageEvent):
     imagebytes = image.getvalue()
     imageb64 = "base64://" + base64.b64encode(imagebytes).decode("utf-8")
     await adapter.send(ImageSegment(file=imageb64))
+    # do upload
+    return
+    if not tfh:
+        return
+    logger.info(
+        f"uploading quote from {sender.user_id}({sender.nickname}) in group {event.group_id}"
+    )
+    logger.debug(f"ensuring folder existence: {upload_folder}")
+    folder_id = None
+    if (rootdirs := await list_group_rootdirs(adapter, event.group_id)) is None:
+        logger.error("error when checking folder existence")
+        return
+    else:
+        for folder in rootdirs["folders"]:
+            if folder["folder_name"] == upload_folder:
+                folder_id = folder["folder_id"]
+                break
+    if not folder_id:
+        logger.info("target folder not exists, using rootdir")
+        folder_id = "/"
+        # logger.info("target folder not exists, try creating")
+        # creation_result = cast(
+        #     Echo,
+        #     await (
+        #         await adapter.with_echo(adapter.call_output)(
+        #             CreateGroupFileFolderAction(
+        #                 group_id=event.group_id, name=upload_folder, parent_id=None
+        #             )
+        #         )
+        #     )[0],
+        # )
+    async with tfh.tmpsession(None, data=imagebytes) as rmtpath:
+        result: Echo = await (
+            await adapter.with_echo(adapter.call_output)(
+                UploadGroupFileAction(
+                    group_id=event.group_id,
+                    file=rmtpath,
+                    name=f"{time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())}_{sender.user_id}.png",
+                    folder=upload_folder,
+                )
+            )
+        )[0]
+    logger.debug(f"upload result: {result}")
+    if not result.ok:
+        logger.warning("failed to upload")
+
+
+async def list_group_rootdirs(adapter: Adapter, group_id: int):
+    rootdirs = cast(
+        Echo,
+        await (
+            await adapter.with_echo(adapter.call_output)(
+                GetGroupRootFilesAction(group_id)
+            )
+        )[0],
+    )
+    if rootdirs.ok:
+        return rootdirs.data
+    else:
+        return None
+
+
+async def create_folder():
+    pass
 
 
 class Quoter(Plugin):
