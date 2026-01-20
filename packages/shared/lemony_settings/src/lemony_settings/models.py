16313e4e20806554b0add075b402659a12b324fb
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2026-01-20 23:08:59 +0800

    refactor: preparing for new start 4/NaN

--- packages/shared/lemony_settings/src/lemony_settings/models.py
+++ packages/shared/lemony_settings/src/lemony_settings/models.py
@@ -1,99 +1,137 @@
 import textwrap
-from pathlib import Path
-from typing import Literal, TypedDict
+from typing import Any
 
-from melobot.utils import singleton
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationInfo, model_validator
 
 
-class Rule(BaseModel):
-    action: Literal["allow", "deny"] = Field(
-        description="规则的动作, 允许或拒绝.",
-    )
-    command: str | None = Field(
-        default=None,
-        description="适用此规则的命令名称. 若为 None, 则适用于所有命令.",
-    )
-    ids: list[int] = Field(
-        default_factory=list, description="适用此规则的用户或群组ID列表."
-    )
-    deny_message: str | None = Field(
-        default=None,
-        description="允许为单个规则指定拒绝消息, 覆盖全局配置.",
-    )
-    # 定义见下
+class BaseSettings(BaseModel):
+    # 给所有设置模型定义一个基类, 以便于类型检查和识别.
+    # 每个字段都需要有一个默认值, 以确保设置模型可以被正确初始化.
+    def model_post_init(self, context: Any) -> None:
+        return super().model_post_init(context)
 
 
-class RuleConfig(BaseModel):
-    private: list[Rule] = Field(default_factory=list, description="私聊相关的规则列表.")
-    group: list[Rule] = Field(default_factory=list, description="群聊相关的规则列表.")
-
-
-class GlobalConfig(BaseModel):
-    mode: Literal["whitelist", "blacklist"] = Field(
-        default="blacklist",
+class LemonySettings[SettingModelT: BaseSettings](BaseModel):
+    identifier: str = Field(
         description=textwrap.dedent(
             """
-            全局配置的模式, 决定未在插件中指定配置时的默认行为. 可被插件配置覆盖.
-            `whitelist`: 仅允许在插件中明确允许的操作.
-            `blacklist`: 仅禁止在插件中明确禁止的操作.
+            设置的唯一标识符. 遵循变量名命名规范.
+            插件名/模块名, etc.
+            """,
+        ).strip(),
+        frozen=True,
+        pattern=r"^[A-Za-z_][A-Za-z0-9_]*$",
+    )
+    namespace: str = Field(
+        default="default",
+        description=textwrap.dedent(
+            """
+            设置的命名空间.
+            用于区分同一插件/模块的不同设置文件.
+            """,
+        ).strip(),
+        frozen=True,
+        pattern=r"^[A-Za-z_][A-Za-z0-9_]*$",
+    )
+    # 试图初始化两个 (id, namespace) 相同的 LemonySettings 实例, 会导致冲突.
+    # 此时应该抛出异常. (在更上层)
+
+    model: type[SettingModelT] = Field(
+        description=textwrap.dedent(
+            """
+            设置值的模型类型.
+            用于反序列化和验证.
+            """,
+        ).strip(),
+        frozen=True,
+    )
+    value: SettingModelT = Field(
+        default=None,  # type: ignore
+        # 这个值将在下面的 model_validator 中依据 model 字段的值被正确初始化.
+        description=textwrap.dedent(
+            """
+            设置的值.
+            模型由使用此设置的插件或模块定义.
+            """,
+        ).strip(),
+        frozen=True,
+        validate_default=False,
+    )
+
+    @model_validator(mode="before")
+    @classmethod
+    def _init_value(cls, data: dict[str, Any], info: ValidationInfo) -> dict[str, Any]:
+        if not issubclass((model := data["model"]), BaseSettings):
+            raise TypeError("model must be a subclass of BaseSettings")
+        # 不允许直接把 BaseSettings 作为 model 类型.
+        if model is BaseSettings:
+            raise TypeError(
+                "model cannot be BaseSettings directly, inherit a subclass instead"
+            )
+
+        # 如果 value 是 None, 则使用 model 的默认值初始化.
+        # 这就是为什么 BaseSettings 的字段必须有默认值.
+        if (value := data.get("value")) is None:
+            try:
+                model_instance = model()
+            except Exception as e:
+                raise ValueError(
+                    f"Failed to initialize value with default model: {e}"
+                ) from e
+            data["value"] = model_instance
+        elif not isinstance(value, model):
+            raise TypeError(f"Expected value of type {model}, got {type(value)}")
+        return data
+
+
+class GlobalSettings(BaseModel):
+    auto_save: bool = Field(
+        default=True,
+        description=textwrap.dedent(
+            """
+            是否启用自动保存功能.
+            启用后, 当设置值被修改时, 会自动保存到配置文件中.
             """,
         ).strip(),
     )
-    owner: int | None = Field(
-        default=None,
-        description="机器人所有者的QQ号. 不可被插件配置覆盖. 无视所有权限检查.",
-    )
-    admins: list[int] = Field(
-        default_factory=list,
+    auto_reload: bool = Field(
+        default=False,
         description=textwrap.dedent(
             """
-            机器人的管理员列表. 不可被插件配置覆盖. 拥有较高权限.
-            有一说一这个档位的权限定位有点尴尬, 看插件自己怎么用.
-            """,
-        ),
-    )
-    rules: RuleConfig = Field(
-        default_factory=RuleConfig,
-        description=textwrap.dedent(
-            """
-            全局权限规则列表. 优先于插件配置的规则列表.
-            在此基础上每个插件可以有自己的规则列表, 用于细粒度的权限控制.
-            """,
-        ),
-        # 按顺序应用规则, 直到匹配为止.
-        # 如果没有规则匹配, 则根据 mode 决定允许或拒绝.
-    )
-    deny_message: str | None = Field(
-        default=None,
-        description=textwrap.dedent(
-            """
-            当操作被拒绝时发送的消息内容.
-            若为`None`, 则不发送任何消息. (静默拒绝)
-            可被插件配置覆盖.
-            """,
-        ).strip(),
-    )  # 可以被 checker 初始化时候传入的 deny_cb 覆盖
-
-
-class PluginConfig(BaseModel):
-    mode: Literal["whitelist", "blacklist"] | None = Field(default=None)
-    rules: RuleConfig = Field(
-        default_factory=RuleConfig,
-        description=textwrap.dedent(
-            """
-            插件特定的权限规则列表.
-            优先于全局配置的规则列表.
-            """,
-        ),
-    )
-    deny_message: str | None = Field(
-        default=None,
-        description=textwrap.dedent(
-            """
-            当操作被拒绝时发送的消息内容.
-            若为`None`, 则不发送任何消息. (静默拒绝)
-            优先于全局配置的 deny_message.
+            是否启用自动重载功能.
+            启用后, 当设置值在配置文件中被修改时, 会自动重新加载.
             """,
         ).strip(),
     )
+
+
+# 最终期望的文件目录结构:
+# configs/  # 或许可以接收命令行参数来指定别的目录
+#   global.toml
+#   plugin_a/ # identifier
+#     a.toml  # namespace.toml
+#     b.toml
+#   plugin_b/
+#     c.toml
+#   module_x/
+#     x.toml
+
+
+SETTINGS_TABLE: dict[tuple[str, str], LemonySettings] = {}
+
+
+def require[T: BaseSettings](
+    identifier: str, model: type[T], namespace: str = "default"
+) -> T:
+    """
+    获取一个 LemonySettings 实例. 如果不存在则创建一个新的实例并返回其值.
+    """
+    key = (identifier, namespace)
+    if key not in SETTINGS_TABLE:
+        setting = LemonySettings(
+            identifier=identifier,
+            namespace=namespace,
+            model=model,
+        )
+        SETTINGS_TABLE[key] = setting
+    return SETTINGS_TABLE[key].value
