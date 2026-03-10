a37847c40597adb6f05a953c093bff8393dc7375
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2026-03-10 21:10:15 +0800

    refactor: load pinyin to katakana mapping from resources instead of file path

--- packages/shared/lemony_utils/src/lemony_utils/pinyin.py
+++ packages/shared/lemony_utils/src/lemony_utils/pinyin.py
@@ -1,3 +1,4 @@
+import importlib.resources
 import json
 from os import PathLike
 
@@ -6,11 +7,15 @@ from pypinyin import Style, pinyin
 PY2KTKN_MAP: dict[str, str] = {}
 
 
-def load_py2ktkn_map(data_path: PathLike | str = "data/pinyin2ktkn.json") -> None:
+def load_py2ktkn_map() -> None:
     global PY2KTKN_MAP
+
     # https://github.com/RUI-LONG/Python-Pinyin-Kana/blob/70f93061786ce538a2b26798e67fa501fd9b9867/pinyin_kana/pinyin_dicts.py#L40
-    with open(data_path, "r", encoding="utf-8") as fp:
-        PY2KTKN_MAP = json.load(fp)
+    PY2KTKN_MAP = json.loads(
+        importlib.resources.files("lemony_utils.resources")
+        .joinpath("py2ktkn_map.json")
+        .read_text(encoding="utf-8")
+    )
 
 
 def _convert_to_katakana(text, ignore_non_kana=False):
@@ -29,11 +34,3 @@ def pinyin_to_katakana(text: str | list[str]) -> list[str]:
     if isinstance(text, list):
         return ["".join(_convert_to_katakana(t)) for t in text]
     return _convert_to_katakana(text)
-
-
-if __name__ == "__main__":
-    load_py2ktkn_map()
-    chinese_text = "你好ww,, wq我叫ww"
-    katakana_text = pinyin_to_katakana(chinese_text)
-    print("原文:", chinese_text)
-    print("片假名:", katakana_text)
