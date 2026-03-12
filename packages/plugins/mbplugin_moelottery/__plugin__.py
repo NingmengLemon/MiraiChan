4fa554fcae7e150a58523ff18ba5c3baac9c9add
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2026-03-12 09:21:46 +0800

    feat: support group isolation

--- packages/plugins/mbplugin_moelottery/__plugin__.py
+++ packages/plugins/mbplugin_moelottery/__plugin__.py
@@ -18,6 +18,7 @@ PLUGIN_IDENTIFIER = "moelottery"
 
 class MoeLotConfig(BaseSettings):
     moedata_file: str | None = None
+    group_isolation: bool = False  # 是否启用群隔离, 启用后每个群的抽取结果互不影响
 
 
 cfgloader = require(
@@ -26,7 +27,9 @@ cfgloader = require(
 )
 logger = get_logger()
 moelot = LotteryBox(cfgloader.value.moedata_file)
-cd_table: dict[int, str] = {}  # user_id -> date_str (精度为天), 用于冷却
+cd_table: dict[
+    tuple[int, int], str
+] = {}  # (user_id, group_id) -> date_str (精度为天), 用于冷却
 checker_factory = get_checker_factory_wrapper(
     plugin_name=PLUGIN_IDENTIFIER,
 )
@@ -44,12 +47,14 @@ async def draw_attrs(event: GroupMessageEvent, adapter: Adapter, logger: Generic
         # sender_id 为 None 是旧 QQ 的匿名用户, 现代 QQ 已经没有了
         # 但是我们亲爱的 OneBot11 协议仍然保留了这个特性, 以至于我们不得不在这里处理一下
         return  # 直接静默拒绝
-    if cd_table.get(event.sender.user_id) == (
+    gid = event.group_id if cfgloader.value.group_isolation else 0
+    uid = event.sender.user_id
+    if cd_table.get((uid, gid)) == (
         now_date := time.strftime("%Y-%m-%d", time.localtime())
-    ) and checker_factory.is_owner(event.sender.user_id):
+    ) and not checker_factory.is_owner(event.sender.user_id):
         await adapter.send_reply("今天已经抽过了噢")
         return
-    cd_table[event.sender.user_id] = now_date
+    cd_table[(uid, gid)] = now_date
     moeattr = moelot.lot()
     logger.debug(f"{moeattr=}")
     await adapter.send_reply(f"你今天的人设是{moelot.build_response_text(moeattr)}！")
