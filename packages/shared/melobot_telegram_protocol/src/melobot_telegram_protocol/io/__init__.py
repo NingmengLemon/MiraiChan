23b9bbcb17fbe87e359ebd9b6a6b77ce72c895e3
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2026-06-01 13:07:24 +0800

    rm unused & badly impl-ed melobot_telegram_protocol for future rewrite

--- packages/shared/melobot_telegram_protocol/src/melobot_telegram_protocol/io/__init__.py
+++
@@ -1,9 +0,0 @@
-from .packet import EchoPacket, InPacket, OutPacket
-from .source import TelegramPollingIO
-
-__all__ = [
-    "EchoPacket",
-    "InPacket",
-    "OutPacket",
-    "TelegramPollingIO",
-]
